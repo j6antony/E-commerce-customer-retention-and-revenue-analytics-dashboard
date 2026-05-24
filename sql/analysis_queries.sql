79b114a506b10940aa531bc7f9086149652a84c9
Johan Antony <j6antony@uwaterloo.ca>
2026-05-24 15:34:30 -0400

    fix again

--- sql/analysis_queries.sql
+++ sql/analysis_queries.sql
@@ -60,8 +60,8 @@ FROM (
     SELECT
         c.customer_unique_id,
         COUNT(DISTINCT o.order_id) AS order_count
-    FROM order o
-    JOIN customer c
+    FROM orders o
+    JOIN customers c
         ON o.customer_id = c.customer_id
     WHERE o.order_status = 'delivered'
     GROUP BY c.customer_unique_id
